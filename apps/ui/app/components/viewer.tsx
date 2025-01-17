e71d0f0e8c1852c7c2d607897efb33805bac94cd
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-01-18 10:49:23 +1300

    feat(ui): update viewer component to load 3D models

--- apps/ui/app/components/viewer.tsx
+++ apps/ui/app/components/viewer.tsx
@@ -1,41 +1,32 @@
-import { useRef, useState } from 'react'
-import { Canvas, useFrame, MeshProps } from '@react-three/fiber'
+import { Canvas, MeshProps } from '@react-three/fiber'
+import { useGLTF, OrbitControls } from '@react-three/drei'
 
-function Box(properties: any) {
-    // This reference gives us direct access to the THREE.Mesh object
-    const reference = useRef<MeshProps>(null)
-    // Hold state for hovered and clicked events
-    const [hovered, hover] = useState(false)
-    const [clicked, click] = useState(false)
-    // Subscribe this component to the render-loop, rotate the mesh every frame
-    // useFrame((state, delta) => {
-    //     if (ref.current) {
-    //         ref.current.rotation.x += delta;
-    //     }
-    // })
-    // Return the view, these are regular Threejs elements expressed in JSX
-    return (
-        <mesh
-            {...properties}
-            ref={reference}
-            rotation={[0, 45, 0]}
-            scale={clicked ? 1.5 : 1}
-            onClick={(event) => click(!clicked)}
-            onPointerOver={(event) => hover(true)}
-            onPointerOut={(event) => hover(false)}>
-            <boxGeometry args={[1, 1, 1]} />
-            {/* @ts-expect-error - color is incorrectly typed */}
-            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-        </mesh>
-    )
+const MODELS = {
+    Beech: 'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/tree-beech/model.gltf',
+    Lime: 'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/tree-lime/model.gltf',
+    Spruce: 'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/tree-spruce/model.gltf',
+    Camera: 'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/refs/heads/main/2.0/AntiqueCamera/glTF/AntiqueCamera.gltf'
 }
 
+const Model = ({ url, ...properties }: { url: string } & MeshProps) => {
+    const { scene } = useGLTF(url);
+    return <primitive object={scene} {...properties} />;
+};
+
 export default function Viewer() {
-    return <Canvas>
+    // useDeferredValue allows us to defer updates, the component is market by React
+    // so that it does *not* run into the fallback when something new loads
+    // const deferred = useDeferredValue(MODELS.Beech)
+    // We can find out the loading state by comparing the current value with the deferred value
+    // const isLoading = url !== deferred
+    // const { scene } = useGLTF(deferred)
+
+    return <Canvas camera={{ position: [-10, 20, 40], fov: 10 }}>
         <ambientLight intensity={Math.PI / 2} />
         <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
         <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-        <Box position={[0, 1, 0]} />
-        <Box position={[0, -1, 0]} />
+        <Model url={MODELS.Camera} position={[0, -5, 0]} rotation={[0.1, -5.7, 0]} />
+
+        <OrbitControls />
     </Canvas>;
 }

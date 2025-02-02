d7253bf232e2a7b282a12b643dc975e87c9857c3
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-02-03 11:54:43 +1300

    fix(ui): update Dockerfile and manifest handling

--- apps/ui/app/root.tsx
+++ apps/ui/app/root.tsx
@@ -77,7 +77,7 @@ export function App() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
-        <ManifestLink />
+        <ManifestLink manifestUrl="/manifest.json" />
         <Links />
       </head>
       <body className="overflow-hidden">

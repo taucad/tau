2ad54a0a966849b83bc256e210627baa7f331ce3
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-02-09 15:30:43 +1300

    refactor(ui): improve loading state positioning and styles in ReplicadStudio

--- apps/ui/app/components/geometry/replicad/replicad-studio.tsx
+++ apps/ui/app/components/geometry/replicad/replicad-studio.tsx
@@ -36,21 +36,23 @@ export function ReplicadStudio() {
   }, [dispatch]);
 
   return (
-    <div className="flex flex-col h-full">
-      <ReplicadViewer mesh={mesh} />
-      {/* Loading state, only show when mesh is loaded and computing */}
-      {mesh && (isComputing || isBuffering) && (
-        <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
-          <div className="m-auto flex items-center gap-2 bg-background/50 backdrop-blur-sm p-2 rounded-md">
-            <span className="text-sm font-mono text-muted-foreground">
-              {isBuffering ? 'Buffering...' : 'Rendering...'}
-            </span>
-            <LoaderPinwheel className="w-6 h-6 animate-spin text-primary ease-in-out" />
+    <>
+      <div className="relative flex flex-col h-full">
+        <ReplicadViewer mesh={mesh} />
+        {/* Loading state, only show when mesh is loaded and computing */}
+        {mesh && (isComputing || isBuffering) && (
+          <div className="absolute top-[80%] left-[50%] -translate-x-[50%] -translate-y-[80%]">
+            <div className="m-auto flex items-center gap-2 bg-background/70 border border-neutral-200 backdrop-blur-sm p-2 rounded-md">
+              <span className="text-sm font-mono text-muted-foreground">
+                {isBuffering ? 'Buffering...' : 'Rendering...'}
+              </span>
+              <LoaderPinwheel className="w-6 h-6 animate-spin text-primary ease-in-out" />
+            </div>
           </div>
+        )}
+        <div className="absolute bottom-0 left-0">
+          {error && <div className="text-destructive p-0.5 rounded-tr-md bg-destructive/10 text-xs">{error}</div>}
         </div>
-      )}
-      <div className="absolute bottom-0 left-0">
-        {error && <div className="text-destructive p-0.5 rounded-tr-md bg-destructive/10 text-xs">{error}</div>}
       </div>
       <div className="absolute top-0 right-0 flex flex-row justify-end gap-1.5 m-1.5 mr-12">
         <Collapsible>
@@ -114,6 +116,6 @@ export function ReplicadStudio() {
           className="text-muted-foreground"
         />
       </div>
-    </div>
+    </>
   );
 }

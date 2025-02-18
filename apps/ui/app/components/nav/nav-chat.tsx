7d62718b30ef5adac04f38ba18385dd2af5a65d6
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-02-19 09:09:48 +1300

    style(ui): Add new build sidebar styles

--- apps/ui/app/components/nav/nav-chat.tsx
+++ apps/ui/app/components/nav/nav-chat.tsx
@@ -1,18 +1,20 @@
 import { SidebarMenuButton } from '@/components/ui/sidebar';
 import { Plus } from 'lucide-react';
 import { SidebarGroup } from '../ui/sidebar';
-import { Link } from '@remix-run/react';
+import { NavLink } from '@remix-run/react';
 
 export const NavChat = () => {
   return (
     // Elevate the sidebar group above the other items to ensure the new build button is always clickable
     <SidebarGroup className="z-10">
-      <Link to="/builds/new">
-        <SidebarMenuButton tooltip="New Build" variant="outline">
-          <Plus />
-          <span>New Build</span>
-        </SidebarMenuButton>
-      </Link>
+      <NavLink to="/builds/new">
+        {({ isActive }) => (
+          <SidebarMenuButton isActive={isActive} tooltip="New Build" variant="outline">
+            <Plus />
+            <span>New Build</span>
+          </SidebarMenuButton>
+        )}
+      </NavLink>
     </SidebarGroup>
   );
 };

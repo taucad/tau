37a9b9b4fee6a8000def5e095a8fcec912c20682
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-02-22 17:25:36 +1300

    refactor(ui): update color picker layout and styles

--- apps/ui/app/components/ui/color-picker.tsx
+++ apps/ui/app/components/ui/color-picker.tsx
@@ -55,45 +55,32 @@ const ColorPicker = forwardRef<
         </PopoverTrigger>
         <TooltipContent side="right">Choose theme color</TooltipContent>
       </Tooltip>
-      <PopoverContent side="right" className="w-full flex flex-col gap-2 px-1">
+      <PopoverContent side="right" className="w-48 flex flex-row gap-4 px-0 py-3">
         <Slider
           min={0}
           max={360}
           value={[value.h]}
           onValueChange={([h]) => handleChange({ h, s: 50, l: 50 })}
           className={cn(
-            //
             '[&_[data-slot="range"]]:bg-transparent',
             '[&_[data-slot="track"]]:bg-[linear-gradient(_to_right,_oklch(var(--l-primary)_var(--c-primary)_0),_oklch(var(--l-primary)_var(--c-primary)_120),_oklch(var(--l-primary)_var(--c-primary)_240),_oklch(var(--l-primary)_var(--c-primary)_360)_)]',
             '[&_[data-slot="track"]]:h-6',
             '[&_[data-slot="track"]]:mx-3',
             '[&_[data-slot="track"]]:rounded-md',
             '[&_[data-slot="thumb"]]:bg-primary',
-            '[&_[data-slot="thumb"]]:size-8',
+            '[&_[data-slot="thumb"]]:size-9',
             '[&_[data-slot="thumb"]]:border-border',
             '[&_[data-slot="thumb"]]:border-2',
           )}
         />
-        <div className="flex items-center gap-2 mx-3">
-          <Input
-            min={0}
-            max={360}
-            type="number"
-            onChange={(event) => {
-              handleChange({ h: Number(event?.currentTarget?.value), s: 50, l: 50 });
-            }}
-            ref={forwardedReference}
-            value={value.h}
-          />
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button variant="outline" size="icon" onClick={onReset}>
-                <RotateCcw className="size-4" />
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent>Reset</TooltipContent>
-          </Tooltip>
-        </div>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button variant="outline" size="icon" onClick={onReset} className="-ml-3 mr-3 w-12">
+              <RotateCcw className="size-4" />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>Reset</TooltipContent>
+        </Tooltip>
       </PopoverContent>
     </Popover>
   );

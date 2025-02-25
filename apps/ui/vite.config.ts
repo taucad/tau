b607c9f441aa48948136db4679195aafc0377203
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-02-25 21:10:52 +1300

    feat(ui): implement base64 loader plugin for vite

--- apps/ui/vite.config.ts
+++ apps/ui/vite.config.ts
@@ -4,17 +4,37 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import { remixPWA } from '@remix-pwa/dev';
 import tailwindcss from '@tailwindcss/vite';
 import ViteSvgSpriteWrapper from 'vite-svg-sprite-wrapper';
-import { defineConfig } from 'vite';
+import { defineConfig, type Plugin } from 'vite';
+import fs from 'node:fs';
 
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+/**
+ * A simple plugin to load files as base64 strings.
+ *
+ * The data encoding for url() imports is not supplied.
+ */
+const base64Loader: Plugin = {
+  name: 'base64-loader',
+  transform(_, id) {
+    const [path, query] = id.split('?');
+    if (query != 'base64') return;
+
+    const data = fs.readFileSync(path);
+    const base64 = data.toString('base64');
+
+    return `export default '${base64}';`;
+  },
+};
+
 export default defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/ui-vite',
   plugins: [
+    base64Loader,
     remix({
       future: {
         v3_relativeSplatPath: true,

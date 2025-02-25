b3af350ecc96238c59c5a6037763e8d436e16a0b
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-02-26 10:51:37 +1300

    fix(ui): add type definitions for MatcapMaterial properties

--- apps/ui/app/components/geometry/graphics/three/matcap-material.tsx
+++ apps/ui/app/components/geometry/graphics/three/matcap-material.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useTexture } from '@react-three/drei';
+import { MeshMatcapMaterialProps } from '@react-three/fiber';
 
-export const MatcapMaterial = React.memo(function MatcapMaterial(properties) {
+export const MatcapMaterial = React.memo(function MatcapMaterial(properties: MeshMatcapMaterialProps) {
   const [matcap] = useTexture(['/textures/matcap-1.png']);
   return <meshMatcapMaterial matcap={matcap} {...properties} />;
 });

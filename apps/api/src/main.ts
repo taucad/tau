355200123bf1e8ee3737c315f8beaa2e376a629f
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-03-11 20:25:40 +1300

    feat(api): add body parsing middleware with increased payload size

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,12 +7,15 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { urlencoded, json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'v1';
   app.setGlobalPrefix(globalPrefix);
   app.enableCors();
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);

bb542dc6b7b815d9497407f7575d59726206a079
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-03-25 18:38:39 +1300

    style(ui): Make resizable handles consistent

--- apps/ui/app/components/ui/sidebar.tsx
+++ apps/ui/app/components/ui/sidebar.tsx
@@ -208,7 +208,7 @@ function Sidebar({
           // Adjust the padding for floating and inset variants.
           variant === 'floating' || variant === 'inset'
             ? 'p-2 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4))+2px)]'
-            : 'group-data-[collapsible=icon]:w-(--sidebar-width-icon) group-data-[side=left]:border-r group-data-[side=right]:border-l',
+            : 'group-data-[collapsible=icon]:w-(--sidebar-width-icon)',
           className,
         )}
         {...properties}
@@ -259,7 +259,7 @@ function SidebarRail({ className, ...properties }: React.ComponentProps<'button'
       onClick={toggleSidebar}
       title="Toggle Sidebar"
       className={cn(
-        'hover:after:bg-sidebar-border absolute inset-y-0 z-20 hidden w-4 -translate-x-1/2 transition-all ease-linear group-data-[side=left]:-right-4 group-data-[side=right]:left-0 after:absolute after:inset-y-0 after:left-1/2 after:w-[2px] sm:flex',
+        'after:bg-border absolute inset-y-0 z-20 hidden w-4 -translate-x-1/2 after:-translate-x-1/2 transition-[width] ease-linear group-data-[side=left]:-right-4 group-data-[side=right]:left-0 after:absolute after:inset-y-0 after:left-1/2 after:w-px hover:after:w-[3px] active:after:w-[3px] active:after:bg-neutral/50 after:transition-[width] after:ease-in-out after:duration-200 sm:flex',
         'in-data-[side=left]:cursor-w-resize in-data-[side=right]:cursor-e-resize',
         '[[data-side=left][data-state=collapsed]_&]:cursor-e-resize [[data-side=right][data-state=collapsed]_&]:cursor-w-resize',
         'hover:group-data-[collapsible=offcanvas]:bg-sidebar group-data-[collapsible=offcanvas]:translate-x-0 group-data-[collapsible=offcanvas]:after:left-full',

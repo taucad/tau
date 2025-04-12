217441d7da61e0d05fcca5eb7219a260608c5fa6
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-04-13 11:47:11 +1200

    refactor(root): migrate ESLint configuration to XO

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,148 +1,28 @@
-import nx from '@nx/eslint-plugin';
-import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import xo from 'xo';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import eslintPluginSonarjs from 'eslint-plugin-sonarjs';
-import eslintPluginComments from '@eslint-community/eslint-plugin-eslint-comments/configs';
 
+// Export default xo.xoToEslintConfig([{space: true, react: true, prettier: 'compat'}]);
 /**
  * @type {import('eslint').Linter.Config[]}
  */
-export default [
-  ...nx.configs['flat/base'],
-  ...nx.configs['flat/typescript'],
-  ...nx.configs['flat/javascript'],
-  ...nx.configs['flat/react'],
-  eslintPluginUnicorn.configs['flat/all'],
-  eslintPluginSonarjs.configs.recommended,
-  eslintPluginComments.recommended,
+const config = [
+  ...xo.xoToEslintConfig([{ space: true, react: true, prettier: 'compat' }]),
   eslintPluginPrettierRecommended,
   {
-    ignores: ['**/dist'],
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     rules: {
-      // Enforcing Dependency Inversion Principle from SOLID
-      '@nx/enforce-module-boundaries': [
-        'error',
-        {
-          enforceBuildableLibDependency: true,
-          allow: [String.raw`^.*/eslint(\.base)?\.config\.[cm]?js$`],
-          depConstraints: [
-            {
-              sourceTag: '*',
-              onlyDependOnLibsWithTags: ['*'],
-            },
-          ],
-        },
-      ],
       // Require a description for each ESLint rule comment. This informs co-authors about the rule and why it is being applied.
       '@eslint-community/eslint-comments/require-description': ['error', { ignore: [] }],
-
-      /* TYPESCRIPT RULES */
-      // Unused variables result in poor code hygiene and should be removed.
-      'no-unused-vars': 'off', // Note: we disable the base rule as it can conflict with the typescript rule.
-      '@typescript-eslint/no-unused-vars': 'error',
-      // Disable type assertions by default as they are typically used to suppress type errors, which is a code smell.
-      // In most cases, a type guard should be used instead.
-      // If you are sure that the type assertion is necessary, disable and add a comment.
-      '@typescript-eslint/consistent-type-assertions': ['error', { assertionStyle: 'never' }],
-      // Usage of `any` is discouraged as it almost always leads to type errors.
-      '@typescript-eslint/no-explicit-any': 'error',
-      // Enforce explicit accessibility modifiers for class members to improve readability and maintainability.
-      '@typescript-eslint/explicit-member-accessibility': 'error',
-      // Enforce that the `type` keyword is used when importing types, e.g. `import { type Foo } from './foo'`.
-      // This ensures the compiler receives a hint to discard type values when they are present in import statements,
-      // alongside explicit, uniform import styles.
-      '@typescript-eslint/consistent-type-imports': [
-        'error',
-        { prefer: 'type-imports', fixStyle: 'separate-type-imports' },
-      ],
-      // Ensure that import type side effects are prevented when using `verbatimModuleSyntax: true`.
-      '@typescript-eslint/no-import-type-side-effects': 'error',
-      // Enforce types to appear on the constructor of a class.
-      '@typescript-eslint/consistent-generic-constructors': ['error', 'constructor'],
-      // Enforce that arrays always use `Array<T>` syntax. This is more readable in human voice as "array of T".
-      // The exception is when types which are just primitive names or type references are used.
-      '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
-      /* END TYPESCRIPT RULES */
-
-      /* IMPORT RULES */
-      // Helpful Warnings
-      'import/export': 'error',
-      'import/no-deprecated': 'error',
-      'import/no-empty-named-blocks': 'error',
-      'import/no-extraneous-dependencies': 'error',
-      'import/no-mutable-exports': 'error',
-      'import/no-named-as-default': 'error',
-      'import/no-named-as-default-member': 'error',
-      'import/no-unused-modules': 'error',
-      // Module Systems
-      'import/no-commonjs': 'error',
-      // Static Analysis
-      'import/default': 'error',
-      'import/named': 'off', // Disabled as we use `tsconfig` to resolve paths.
-      'import/namespace': 'error',
-      'import/no-absolute-path': 'error',
-      'import/no-cycle': 'error',
-      'import/no-dynamic-require': 'error',
-      'import/no-internal-modules': 'off', // Disabled as multi-level exports are common for packages.
-      'import/no-relative-packages': 'error',
-      'import/no-relative-parent-imports': 'error',
-      'import/no-restricted-paths': 'error',
-      'import/no-self-import': 'error',
-      'import/no-unresolved': 'off', // Disabled as we use `tsconfig` to resolve paths.
-      'import/no-useless-path-segments': 'error',
-      'import/no-webpack-loader-syntax': 'off', // Disabled as we don't use webpack.
-      // Style
-      'import/consistent-type-specifier-style': ['error', 'prefer-top-level'],
-      'import/dynamic-import-chunkname': 'off', // Disabled as we don't use dynamic imports.
-      'import/exports-last': 'error',
-      'import/extensions': 'off', // This rule is difficult to configure correctly given the vast number of extensions. Reconsider later.
-      'import/first': 'error',
-      'import/group-exports': 'off', // Disabled as it's easier to export inline.
-      'import/max-dependencies': 'error',
-      'import/newline-after-import': 'error',
-      'import/no-anonymous-default-export': 'error',
-      'import/no-default-export': 'error', // We prefer named exports.
-      'import/no-duplicates': 'error',
-      'import/no-named-default': 'error',
-      'import/no-named-export': 'off', // We prefer named exports.
-      'import/no-namespace': 'error',
-      'import/no-unassigned-import': 'error',
-      'import/order': 'error',
-      'import/prefer-default-export': 'off', // We prefer named exports.
-      /* END IMPORT RULES */
-
-      /* SONAR fix/to-do rules - will be addressed before release */
-      'sonarjs/fixme-tag': 'off',
-      'sonarjs/todo-tag': 'off',
-    },
-    linterOptions: {
-      // Ensure that unused eslint-disable directives are reported as errors so they can be removed.
-      reportUnusedDisableDirectives: 'error',
-    },
-    languageOptions: {
-      parserOptions: {
-        emitDecoratorMetadata: true,
-        experimentalDecorators: true,
-      },
-    },
-  },
-  {
-    files: ['**/*.tsx', '**/*.jsx'],
-    // Override or add rules here
-    rules: {
-      'unicorn/no-keyword-prefix': 'off',
     },
   },
   {
     // UI App
-    files: ['apps/ui/**/*.ts', 'apps/ui/**/*.tsx', 'apps/ui/**/*.js', 'apps/ui/**/*.jsx'],
-    // Override or add rules here
+    files: ['apps/ui/**/*.ts', 'apps/ui/**/*.tsx'],
     rules: {
       // Turned off as the UI app has a `package.json` required by NX, and this results in a false positive.
-      'import/no-extraneous-dependencies': 'off',
+      'import-x/no-extraneous-dependencies': 'off',
+      'n/no-extraneous-import': 'off',
     },
   },
 ];
+
+export default config;

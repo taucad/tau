b8bcc988f77f3190e92415e28596b4ede9f8801e
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-04-19 10:56:45 +1200

    style(ui): update MatcapMaterial component syntax

--- apps/ui/app/components/geometry/graphics/three/matcap-material.tsx
+++ apps/ui/app/components/geometry/graphics/three/matcap-material.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { useTexture } from '@react-three/drei';
 import * as THREE from 'three';
 
-export const MatcapMaterial = React.memo(function MatcapMaterial(
-  properties: React.ComponentProps<'meshMatcapMaterial'>,
-) {
+export const MatcapMaterial = React.memo(function (properties: React.ComponentProps<'meshMatcapMaterial'>) {
   const [matcap] = useTexture(['/textures/matcap-1.png']);
   matcap.colorSpace = THREE.SRGBColorSpace;
+  // eslint-disable-next-line react/no-unknown-property -- TODO: make Three.js type available for linter
   return <meshMatcapMaterial matcap={matcap} {...properties} />;
 });

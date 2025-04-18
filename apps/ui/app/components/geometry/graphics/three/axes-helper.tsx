f72f0292c5d04d514f127cb646670706aa56d786
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-04-19 10:58:25 +1200

    refactor(ui): enhance AxesHelper component definition

--- apps/ui/app/components/geometry/graphics/three/axes-helper.tsx
+++ apps/ui/app/components/geometry/graphics/three/axes-helper.tsx
@@ -1,3 +1,6 @@
-export function AxesHelper() {
+import type { JSX } from 'react';
+
+export function AxesHelper(): JSX.Element {
+  // eslint-disable-next-line react/no-unknown-property -- TODO: make Three.js type available for linter
   return <axesHelper args={[5000]} userData={{ isPreviewOnly: true }} />;
 }

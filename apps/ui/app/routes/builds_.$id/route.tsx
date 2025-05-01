c97fcc41ba2439045e4371c4ff0b535d0800170f
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-05-02 09:55:12 +1200

    feat(ui): remove console log from chat onFinish handler

--- apps/ui/app/routes/builds_.$id/route.tsx
+++ apps/ui/app/routes/builds_.$id/route.tsx
@@ -115,9 +115,6 @@ function Chat() {
   const { build, isLoading, setMessages: setBuildMessages } = useBuild();
   const { setCode, setParameters } = useReplicad();
   const { setMessages, messages, reload, status, addToolResult } = useAiChat({
-    onFinish(message, options) {
-      console.log('onFinish', message, options);
-    },
     onToolCall: new Map([
       [
         'file_edit',

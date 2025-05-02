d33df8a1cc004a64dbcfefec68763b6bfab8a9e4
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-05-02 17:21:30 +1200

    style(ui): improve chat error message display

--- apps/ui/app/routes/builds_.$id/chat-error.tsx
+++ apps/ui/app/routes/builds_.$id/chat-error.tsx
@@ -8,8 +8,9 @@ export const ChatError = memo(() => {
   if (!error) return null;
 
   return (
-    <div className="flex w-full flex-wrap items-center justify-center gap-1 rounded-md bg-destructive/5 p-2 text-center text-sm">
-      <p className="my-1">Unable to send the message. {error.message}.</p>
+    <div className="flex w-full flex-col items-center justify-center gap-1 rounded-md border border-destructive/20 bg-destructive/10 p-2 text-center text-sm">
+      <p className="my-1">Unable to send the message.</p>
+      <pre className="text-xs">{error.message}</pre>
       <Button variant="link" className="inline-block h-auto p-0" onClick={async () => reload()}>
         Retry
       </Button>

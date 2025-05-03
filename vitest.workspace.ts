8325bfa0985476243a096467d00048154aaf21c1
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-05-03 23:52:18 +1200

    refactor(root): update vitest workspace configuration

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,1 +1,9 @@
-export default ['**/*/vite.config.ts', '**/*/vitest.config.ts'];
+import { defineWorkspace } from 'vitest/config';
+
+export default defineWorkspace([
+  {
+    test: {
+      include: ['**/*/vite.config.ts', '**/*/vitest.config.ts'],
+    },
+  },
+]);

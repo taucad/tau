5fb52185efef528f1951740826ad8223d77411f9
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-05-05 15:52:16 +1200

    style(ui): adjust chat error message styling

--- apps/ui/app/routes/builds_.$id/chat-error.tsx
+++ apps/ui/app/routes/builds_.$id/chat-error.tsx
@@ -17,9 +17,9 @@ export const ChatError = memo(() => {
   }
 
   return (
-    <div className="flex w-full flex-col items-start justify-center gap-1 rounded-md border border-destructive/20 bg-destructive/10 px-2 py-1 text-center text-sm">
-      <div className="flex w-full items-center justify-between">
-        <p className="my-1">Unable to send the message.</p>
+    <div className="flex w-full flex-col items-start justify-center gap-1 rounded-md border border-destructive/20 bg-destructive/10 p-2 text-center text-sm">
+      <div className="flex w-full items-start justify-between">
+        <p>Unable to send the message.</p>
         <Button variant="outline" size="xs" className="h-auto p-1" onClick={async () => reload()}>
           <RefreshCcw className="size-3" />
           Retry

53149ba8239270b827f87f57e3751b0628a76266
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-05-08 18:15:21 +1200

    feat(ui): add file edit view with expandable code preview

--- apps/ui/app/routes/builds_.$id/chat-message-tool-file-edit.tsx
+++ apps/ui/app/routes/builds_.$id/chat-message-tool-file-edit.tsx
@@ -1,6 +1,7 @@
 import type { ToolInvocationUIPart } from '@ai-sdk/ui-utils';
+import { useState } from 'react';
 import type { JSX } from 'react';
-import { Play } from 'lucide-react';
+import { ChevronDown, File, LoaderCircle, Play } from 'lucide-react';
 import { CodeViewer } from '@/components/code-viewer.js';
 import { CopyButton } from '@/components/copy-button.js';
 import { useBuild } from '@/hooks/use-build2.js';
@@ -10,6 +11,7 @@ import { cn } from '@/utils/ui.js';
 
 export function ChatMessageToolFileEdit({ part }: { readonly part: ToolInvocationUIPart }): JSX.Element {
   const { setCode } = useBuild();
+  const [isExpanded, setIsExpanded] = useState(false);
 
   switch (part.toolInvocation.state) {
     case 'call':
@@ -22,7 +24,16 @@ export function ChatMessageToolFileEdit({ part }: { readonly part: ToolInvocatio
       return (
         <div className="border-neutral-200 @container/code overflow-hidden rounded-md border bg-neutral/10">
           <div className="sticky top-0 flex flex-row items-center justify-between border-b border-neutral/20 py-1 pr-1 pl-3 text-foreground/50">
-            <div className="text-xs">{fileName}</div>
+            <div className="flex flex-row items-center gap-1 text-xs text-muted-foreground">
+              <File className={cn('hidden size-3', part.toolInvocation.state === 'result' && 'block')} />
+              <LoaderCircle
+                className={cn(
+                  'hidden size-3 animate-spin text-primary-foreground',
+                  part.toolInvocation.state === 'partial-call' && 'block',
+                )}
+              />
+              <span>{fileName}</span>
+            </div>
             <div className="flex flex-row gap-1">
               <CopyButton
                 size="xs"
@@ -47,34 +58,35 @@ export function ChatMessageToolFileEdit({ part }: { readonly part: ToolInvocatio
               </Tooltip>
             </div>
           </div>
-          {/* <div className={cn('relative max-h-64', isExpanded ? 'max-h-none' : 'overflow-y-auto')}> */}
-          <div
-            className={cn(
-              'prose w-full max-w-full text-sm text-foreground',
-              '[--tw-prose-headings:text-foreground]',
-              '[--tw-prose-bullets:text-foreground]',
-              '[--tw-prose-bold:text-foreground]',
-              '[--tw-prose-counters:text-foreground]',
-              '[--tw-prose-lead:text-foreground]',
-              '[--tw-prose-quotes:text-foreground]',
-              '[--tw-prose-quote-borders:text-foreground]',
-              '[--tw-prose-kbd:text-foreground]',
-              '[--tw-prose-links:text-foreground]',
-              /* <pre> */
-              'p-0 ps-0 pe-0 text-xs',
-            )}
-          >
-            <CodeViewer language="typescript">{content}</CodeViewer>
-
-            {/* FIXME: ensure the code viewer doesn't auto-collapse during scrolling */}
-            {/* <Button
-                  variant="ghost"
-                  size="xs"
-                  className="h-4 text-foreground/50 sticky bottom-0 w-full text-center rounded-none hover:bg-neutral/20"
-                  onClick={() => setIsExpanded((previous) => !previous)}
-                >
-                  <ChevronDown className={cn('transition-transform', isExpanded ? 'rotate-x-180' : '')} />
-                </Button> */}
+          <div className={cn('relative max-h-32', isExpanded ? 'max-h-none' : 'overflow-y-auto')}>
+            <div
+              className={cn(
+                'prose w-full max-w-full text-sm text-foreground',
+                '[--tw-prose-headings:text-foreground]',
+                '[--tw-prose-bullets:text-foreground]',
+                '[--tw-prose-bold:text-foreground]',
+                '[--tw-prose-counters:text-foreground]',
+                '[--tw-prose-lead:text-foreground]',
+                '[--tw-prose-quotes:text-foreground]',
+                '[--tw-prose-quote-borders:text-foreground]',
+                '[--tw-prose-kbd:text-foreground]',
+                '[--tw-prose-links:text-foreground]',
+                /* <pre> */
+                'p-0 ps-0 pe-0 text-xs leading-0',
+              )}
+            >
+              <CodeViewer language="typescript">{content}</CodeViewer>
+              <Button
+                variant="ghost"
+                size="xs"
+                className="sticky bottom-0 h-4 w-full rounded-none bg-neutral/20 text-center text-foreground/50 hover:bg-neutral"
+                onClick={() => {
+                  setIsExpanded((previous) => !previous);
+                }}
+              >
+                <ChevronDown className={cn('transition-transform', isExpanded ? 'rotate-x-180' : '')} />
+              </Button>
+            </div>
           </div>
         </div>
       );

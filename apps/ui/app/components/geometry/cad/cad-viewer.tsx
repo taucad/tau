fc15aa9ace8e286cccaf0e618d13ffeff967c7fb
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-05-18 23:39:05 +1200

    feat(ui): add SVG viewer for 2D geometric shapes

--- apps/ui/app/components/geometry/cad/cad-viewer.tsx
+++ apps/ui/app/components/geometry/cad/cad-viewer.tsx
@@ -4,6 +4,7 @@ import { ReplicadMesh } from '~/components/geometry/kernel/replicad/replicad-mes
 import { ThreeProvider } from '~/components/geometry/graphics/three/three-context.js';
 import type { ThreeViewerProperties } from '~/components/geometry/graphics/three/three-context.js';
 import type { Shape } from '~/types/cad.js';
+import SvgViewer from '~/components/geometry/kernel/replicad/svg-viewer.js';
 
 type CadViewerProperties = Omit<ThreeViewerProperties, 'enableCameraControls'> & {
   readonly shapes: Shape[];
@@ -19,11 +20,27 @@ export function CadViewer({ shapes, zoomLevel, ...properties }: CadViewerPropert
     );
   }
 
+  const svgShapes = shapes.filter((shape) => shape.type === '2d');
+
+  // If there are any SVG shapes, we render them in a SVG viewer
+  if (svgShapes.length > 0) {
+    return <SvgViewer shapes={svgShapes} />;
+  }
+
   return (
     <ThreeProvider stageOptions={{ perspective: { zoomLevel } }} enableCameraControls={false} {...properties}>
-      {shapes.map((shape) => (
-        <ReplicadMesh key={shape.name} {...shape} />
-      ))}
+      {shapes.map((shape) => {
+        if (shape.type === '3d') {
+          return <ReplicadMesh key={shape.name} {...shape} />;
+        }
+
+        if (shape.type === '2d') {
+          throw new Error('2D shapes are not supported');
+        }
+
+        const neverShape: never = shape;
+        throw new Error(`Unknown shape type: ${JSON.stringify(neverShape)}`);
+      })}
     </ThreeProvider>
   );
 }

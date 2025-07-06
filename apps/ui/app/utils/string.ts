7ae834dd38fcda506ac86a6cab6a99470cc362be
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-07-06 23:35:04 +1200

    refactor(ui): improve camelCaseToSentenceCase string conversion

--- apps/ui/app/utils/string.ts
+++ apps/ui/app/utils/string.ts
@@ -5,7 +5,5 @@
  * @returns The converted Sentence Case string
  */
 export const camelCaseToSentenceCase = (string_: string): string => {
-  return string_.replaceAll(/([A-Z])/g, ' $1').replace(/^./, function (string_) {
-    return string_.toUpperCase();
-  });
+  return string_.replaceAll(/(?<=[a-z\d])([A-Z])/g, ' $1').replace(/^./, (char) => char.toUpperCase());
 };

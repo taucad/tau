fc86cfd2151374e76a699f25615600214dd219f2
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-07-11 15:59:17 +1200

    docs(api): improve constant file documentation

--- apps/api/app/constants/cors.constant.ts
+++ apps/api/app/constants/cors.constant.ts
@@ -5,6 +5,12 @@ import { httpHeader } from '~/constants/http-header.constant.js';
  */
 export const corsMaxAge = 21_600; // 6 hours
 
+/**
+ * CORS allowed headers.
+ */
 export const corsAllowedHeaders = Object.values(httpHeader);
 
+/**
+ * CORS allowed methods.
+ */
 export const corsAllowedMethods = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'];

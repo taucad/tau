9cd28158882470e87f131a93a70d18f3f852c0e7
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-07-30 00:47:22 +1200

    chore(root): update ESLint configuration for monorepo dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -134,6 +134,10 @@ const config = [
           ],
         },
       ],
+
+      // Supporting monorepo with both workspace and project level dependencies.
+      'import-x/no-extraneous-dependencies': ['error', { packageDir: ['.', '../..'] }],
+      'n/no-extraneous-import': 'off', // Disabled as it conflicts with import-x/no-extraneous-dependencies.
     },
   },
   {
@@ -174,9 +178,6 @@ const config = [
     // UI App
     files: ['apps/ui/**/*.ts', 'apps/ui/**/*.tsx'],
     rules: {
-      // Turned off as the UI app has a `package.json` required by NX, and this results in a false positive.
-      'import-x/no-extraneous-dependencies': 'off',
-      'n/no-extraneous-import': 'off',
       // React is a global variable in the UI
       'react/react-in-jsx-scope': 'off',
       'react/boolean-prop-naming': [

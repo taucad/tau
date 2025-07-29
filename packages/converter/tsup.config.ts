998fc447f20c31e0aa63e2e2552af78b311fd09b
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-07-30 00:54:05 +1200

    chore(converter): Scaffold package

---
+++ packages/converter/tsup.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'tsup';
+import type { Options } from 'tsup';
+
+const baseConfig: Options = {
+  entry: ['src/index.ts'],
+  sourcemap: false,
+  clean: true,
+  dts: true,
+  minify: true,
+  minifyWhitespace: true,
+  minifyIdentifiers: true,
+  minifySyntax: true,
+};
+
+export const cjsConfig: Options = {
+  ...baseConfig,
+  format: 'cjs',
+  outDir: 'dist/cjs',
+};
+
+export const esmConfig: Options = {
+  ...baseConfig,
+  format: 'esm',
+  outDir: 'dist/esm',
+};
+
+export default defineConfig([cjsConfig, esmConfig]);

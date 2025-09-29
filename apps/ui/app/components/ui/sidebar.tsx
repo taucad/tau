d774a68984dde1923f3031bbaf6974921e9cee33
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-09-29 14:43:13 +1300

    style(ui): update sidebar trigger cursor styles

--- apps/ui/app/components/ui/sidebar.tsx
+++ apps/ui/app/components/ui/sidebar.tsx
@@ -223,7 +223,7 @@ function SidebarTrigger({ className, onClick, ...properties }: React.ComponentPr
           data-slot="sidebar-trigger"
           variant="ghost"
           size="icon"
-          className={cn('size-7', className)}
+          className={cn('size-7', open ? 'cursor-w-resize' : 'cursor-e-resize', className)}
           onClick={(event) => {
             onClick?.(event);
             toggleSidebar();
@@ -254,12 +254,12 @@ function SidebarRail({ className, ...properties }: React.ComponentProps<'button'
       tabIndex={-1}
       title="Toggle Sidebar"
       className={cn(
-        'absolute inset-y-0 my-5 z-20 opacity-0 hover:opacity-100 hidden w-4 -translate-x-1/2 transition-[width] ease-linear group-data-[side=left]:-right-4 group-data-[side=right]:left-0 after:absolute after:inset-y-0 after:left-1/2 after:w-px after:-translate-x-1/2 after:bg-neutral/50 after:transition-[width] after:duration-200 after:ease-in-out hover:after:w-[3px] hover:after:transition-all active:after:w-[3px] active:after:bg-neutral/50 sm:flex after:rounded-full',
+        'absolute inset-y-0 my-5 z-20 opacity-0 hover:opacity-100 hidden w-4 -translate-x-1/2 transition-[width] ease-linear group-data-[side=left]:-right-3 group-data-[side=right]:left-0 after:absolute after:inset-y-0 after:left-1/2 after:w-px after:-translate-x-1/2 after:bg-neutral/50 after:transition-[width] after:duration-200 after:ease-in-out hover:after:w-[3px] hover:after:transition-all active:after:w-[3px] active:after:bg-neutral/50 sm:flex after:rounded-full',
         'in-data-[side=left]:cursor-w-resize in-data-[side=right]:cursor-e-resize',
         '[[data-side=left][data-state=collapsed]_&]:cursor-e-resize [[data-side=right][data-state=collapsed]_&]:cursor-w-resize',
         'group-data-[collapsible=offcanvas]:translate-x-0 group-data-[collapsible=offcanvas]:after:left-full hover:group-data-[collapsible=offcanvas]:bg-transparent',
-        '[[data-side=left][data-collapsible=offcanvas]_&]:-right-1.5',
-        '[[data-side=right][data-collapsible=offcanvas]_&]:-left-1.5',
+        '[[data-side=left][data-collapsible=offcanvas]_&]:-right-1',
+        '[[data-side=right][data-collapsible=offcanvas]_&]:-left-1',
         className,
       )}
       onClick={toggleSidebar}

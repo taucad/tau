ce52a8105c420457949295c1712ba3aeea6668ec
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-10-01 00:48:09 +1300

    feat(ui): add class name interpolation utility function

--- apps/ui/app/utils/ui.ts
+++ apps/ui/app/utils/ui.ts
@@ -2,6 +2,21 @@ import { clsx } from 'clsx';
 import type { ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+/**
+ * Merge class names using clsx and tailwind-merge.
+ * @param inputs 
+ * @returns the merged class names.
+ */
 export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
+}
+
+/**
+ * Class name interpolation.
+ * @param selector The selector to interpolate.
+ * @param inputs The class names to interpolate.  
+ * @returns the merged class names.
+ */
+export function cni(selector: string, inputs: ClassValue[]): string {
+  return cn(inputs.map((input) => `${selector}:${input}`));
 }

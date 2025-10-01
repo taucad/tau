128914c8eb0bcd64fdd59b72fdc50d1632446dda
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-10-01 13:40:38 +1300

    fix(ui): adjust z-index for docs sidebar

--- apps/ui/app/routes/docs.$/docs-sidebar.tsx
+++ apps/ui/app/routes/docs.$/docs-sidebar.tsx
@@ -93,7 +93,7 @@ export function DocsSidebar({ className }: DocsSidebarProps): React.JSX.Element 
       open={isDocsSidebarOpen}
       onOpenChange={setIsDocsSidebarOpen}
       className={cn( 
-        'w-(--docs-sidebar-width-icon) data-[state=open]:w-full z-10',
+        'w-(--docs-sidebar-width-icon) data-[state=open]:w-full z-20',
         className)}>
       <FloatingPanelToggle
         openIcon={MenuIcon}

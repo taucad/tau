bb0387c2a7a124185d3d6ddb3c57d35fb238e842
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-10-08 17:13:47 +1300

    style(ui): remove break-all from empty items component

--- apps/ui/app/components/ui/empty-items.tsx
+++ apps/ui/app/components/ui/empty-items.tsx
@@ -2,7 +2,7 @@ import { cn } from "#utils/ui.js";
 import { cva, type VariantProps } from "class-variance-authority";
 
 export const emptyItemVariants = cva(
-  "flex flex-col h-full items-center justify-center m-2 border border-dashed text-muted-foreground rounded-xs py-4 px-2 text-center text-sm break-all",
+  "flex flex-col h-full items-center justify-center m-2 border border-dashed text-muted-foreground rounded-xs py-4 px-2 text-center text-sm",
   {
     variants: {
       variant: {

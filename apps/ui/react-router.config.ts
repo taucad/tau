8b0b733cd623554087907e159bd4c5f8884f1121
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-10-10 22:49:38 +1300

    feat(ui): disable prerendering configuration

--- apps/ui/react-router.config.ts
+++ apps/ui/react-router.config.ts
@@ -2,12 +2,10 @@ import type { Config } from '@react-router/dev/config';
 
 export default {
   ssr: true,
-  async prerender() {
-    // Static website content. Renders at build time and is served as static files,
-    // speeds up the first load time.
-    return [
-      // TODO: make FE env-vars available during prerendering.
-      // 'manifest.webmanifest', 'robots.txt', '/llms.txt', '/llms-full.txt'
-    ];
-  },
+  // TODO: make FE env-vars available during prerendering.
+  // async prerender() {
+  //   // Static website content. Renders at build time and is served as static files,
+  //   // speeds up the first load time.
+  //   return ['manifest.webmanifest', 'robots.txt', '/llms.txt', '/llms-full.txt'];
+  // },
 } satisfies Config;

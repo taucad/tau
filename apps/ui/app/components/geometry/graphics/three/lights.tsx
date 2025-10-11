5c236713ef7063b74035441c55e658f6b64d09f2
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-10-11 16:09:09 +1300

    feat(ui): adjust light positioning in three.js scene

--- apps/ui/app/components/geometry/graphics/three/lights.tsx
+++ apps/ui/app/components/geometry/graphics/three/lights.tsx
@@ -4,9 +4,9 @@ export function Lights(): React.JSX.Element {
     <>
       <ambientLight intensity={0.5} />
       <hemisphereLight args={['#ffffff', '#444444', 1]} />
-      <directionalLight position={[-3, -3, 5]} color="white" intensity={2} />
-      <pointLight visible={false} position={[-3, -3, 3]} color="white" intensity={1} />
-      <spotLight visible={false} position={[-3, -3, 4]} color="white" intensity={1} />
+      <directionalLight position={[-1, -3, 5]} color="white" intensity={2} />
+      <pointLight visible={false} position={[-1, -3, 3]} color="white" intensity={1} />
+      <spotLight visible={false} position={[-1, -3, 4]} color="white" intensity={1} />
     </>
   );
 }

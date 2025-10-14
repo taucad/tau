07888c9292b9c1e2333682958c2b9836f2976991
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-10-15 12:37:33 +1300

    refactor(root): update package configuration for converter package

--- packages/converter/tsdown.config.ts
+++ packages/converter/tsdown.config.ts
@@ -7,20 +7,37 @@ const baseConfig: Options = {
   clean: true,
   dts: true,
   minify: true,
-  publicDir: 'src/assets',
+  copy: (options) => [
+    {
+      from: 'src/assets',
+      to: `${options.outDir}/assets`,
+    },
+  ],
   tsconfig: 'tsconfig.build.json',
+  /**
+   * IMPORTANT: This is required for the WASM file paths to be resolved consistently in both
+   * source code and bundled (built) code.
+   *
+   * This is possible via the following pattern:
+   * `new URL('./relative-path', import.meta.url)`
+   *
+   * Note: this causes a minor 2-3% increase in the bundle size.
+   *
+   * @see https://web.dev/articles/bundling-non-js-resources#universal_pattern_for_browsers_and_bundlers
+   */
+  unbundle: true,
 };
 
-export const cjsConfig: Options = {
+const cjsConfig: Options = {
   ...baseConfig,
   format: 'cjs',
   outDir: 'dist/cjs',
 };
 
-export const esmConfig: Options = {
+const esmConfig: Options = {
   ...baseConfig,
   format: 'esm',
   outDir: 'dist/esm',
 };
 
-export default defineConfig([cjsConfig, esmConfig]);
+export default defineConfig([esmConfig, cjsConfig]);

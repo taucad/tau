6c0793d4039f55b07523a3ea5622ed347a4f9e91
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-10-16 23:25:13 +1300

    style(ui): enhance button interaction with scale and transition

--- apps/ui/app/components/ui/button.tsx
+++ apps/ui/app/components/ui/button.tsx
@@ -5,7 +5,7 @@ import type { VariantProps } from 'class-variance-authority';
 import { cn } from '#utils/ui.utils.js';
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-[box-shadow] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-3 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-[box-shadow,transform] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-3 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive active:scale-95 md:active:scale-98 select-none",
   {
     variants: {
       variant: {

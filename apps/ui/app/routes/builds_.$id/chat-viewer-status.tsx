3b49b8deee146224919670786be360c95748dad3
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-10-21 23:06:24 +1300

    fix(ui): adjust chat viewer status positioning

--- apps/ui/app/routes/builds_.$id/chat-viewer-status.tsx
+++ apps/ui/app/routes/builds_.$id/chat-viewer-status.tsx
@@ -9,11 +9,9 @@ export function ChatViewerStatus({ className, ...props }: React.HTMLAttributes<H
   return (
     <div {...props} className={cn(className)}>
       {['buffering', 'rendering', 'booting', 'initializing'].includes(state) ? (
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:top-[90%] md:-translate-y-[90%]">
-          <div className="m-auto flex items-center gap-2 rounded-md border bg-background/70 p-1 backdrop-blur-sm md:p-2">
-            <span className="font-mono text-sm text-muted-foreground capitalize">{state}...</span>
-            <HammerAnimation className="size-4 animate-spin text-primary ease-in-out md:size-6" />
-          </div>
+        <div className="m-auto flex items-center gap-2 rounded-md border bg-background/70 p-1 backdrop-blur-sm md:p-2">
+          <span className="font-mono text-sm text-muted-foreground capitalize">{state}...</span>
+          <HammerAnimation className="size-4 animate-spin text-primary ease-in-out md:size-6" />
         </div>
       ) : null}
     </div>

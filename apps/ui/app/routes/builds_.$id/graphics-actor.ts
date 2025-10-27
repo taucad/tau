596fffcf74b0d1f020f1b30d77fd3cded17ef798
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-10-28 01:06:05 +1300

    feat(ui): add inspector to graphics actor

--- apps/ui/app/routes/builds_.$id/graphics-actor.ts
+++ apps/ui/app/routes/builds_.$id/graphics-actor.ts
@@ -4,11 +4,13 @@ import { cameraCapabilityMachine } from '#machines/camera-capability.machine.js'
 import { fileExplorerMachine } from '#machines/file-explorer.machine.js';
 import { graphicsMachine } from '#machines/graphics.machine.js';
 import { screenshotCapabilityMachine } from '#machines/screenshot-capability.machine.js';
+import { inspect } from '#machines/inspector.js';
 
 export const graphicsActor = createActor(graphicsMachine, {
   input: {
     defaultCameraFovAngle: 60,
   },
+  inspect,
 });
 graphicsActor.start();
 

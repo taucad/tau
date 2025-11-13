b1a6557e7c07b86f4bbd0e5f8f4508c4dc73731e
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-11-13 22:05:11 +1300

    chore(root): set up package configurations for js and kernels

---
+++ packages/js/vitest.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vitest/config';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [nxViteTsPaths(), tsconfigPaths()],
+  test: {
+    environment: 'node',
+    typecheck: {
+      enabled: true,
+      include: ['**/*.test-d.ts'],
+      tsconfig: './tsconfig.spec.json',
+      ignoreSourceErrors: true,
+    },
+    reporters: ['verbose'],
+    setupFiles: ['vitest.setup.ts'],
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: '../../coverage/packages/converter',
+      include: ['src/**/*'],
+      exclude: ['src/**/*.{test,spec}.ts'],
+    },
+  },
+});

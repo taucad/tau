182f19188230fac53403d4c3daf7531ee8c8df5a
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-11-14 02:47:06 +1300

    fix(ui): adjust near plane value in stage configuration

--- apps/ui/app/components/geometry/graphics/three/stage.tsx
+++ apps/ui/app/components/geometry/graphics/three/stage.tsx
@@ -51,7 +51,7 @@ const significantRadiusChangeRatio = 0.1;
 // Default configuration constants
 export const defaultStageOptions = {
   offsetRatio: 3,
-  nearPlane: 1e-6,
+  nearPlane: 1e-3,
   minimumFarPlane: 10_000_000_000,
   farPlaneRadiusMultiplier: 5,
   zoomLevel: 1,

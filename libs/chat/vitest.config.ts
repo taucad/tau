876aa3d509f978dbb46fb4e18ea60b23f9e19d6a
Richard Fontein <32132657+rifont@users.noreply.github.com>
2025-12-03 14:28:42 +1300

    test(root): enable pass with no tests in vitest config

--- libs/chat/vitest.config.ts
+++ libs/chat/vitest.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [nxViteTsPaths(), tsconfigPaths()],
   test: {
+    passWithNoTests: true,
     environment: 'node',
     typecheck: {
       enabled: true,

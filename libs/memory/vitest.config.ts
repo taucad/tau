ebfa2b56f7196b0d29e8e15f04cb66c9778329cc
Richard Fontein <32132657+rifont@users.noreply.github.com>
2026-08-17 15:37:14 +1200

    refactor(memory)!: Move package to internal library

--- libs/memory/vitest.config.ts
+++ libs/memory/vitest.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     reporters: ['verbose'],
     coverage: {
       provider: 'v8',
-      reportsDirectory: '../../coverage/packages/memory',
+      reportsDirectory: '../../coverage/libs/memory',
       include: ['src/**/*'],
       exclude: ['src/**/*.{test,spec,test-d}.ts'],
       thresholds: {
